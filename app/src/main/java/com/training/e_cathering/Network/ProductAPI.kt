81bc6c0a00b7c92b049748e51cc3b71e8fdad871
greatdesigners12 <jackquintine3456@gmail.com>
2022-12-09 09:03:32 +0700

    Tambahan

---
+++ app/src/main/java/com/training/e_cathering/Network/ProductAPI.kt
@@ -0,0 +1,11 @@
+package com.training.e_cathering.Network
+
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+import javax.inject.Singleton
+
+@Singleton
+interface BooksAPI {
+
+}
--- app/src/main/java/com/training/e_cathering/di/AppModule.kt
+++ app/src/main/java/com/training/e_cathering/di/AppModule.kt
@@ -1,4 +1,14 @@
 package com.training.e_cathering.di
 
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 class AppModule {
+
 }
